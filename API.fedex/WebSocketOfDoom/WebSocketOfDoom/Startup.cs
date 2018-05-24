2e900d9421a7783bdf1a3e3de4093e83b506ec06
sam <sagosto@selectintl.com>
2018-05-24 14:49:34 -0400

    Crazy fixes to use azure

--- API.fedex/WebSocketOfDoom/WebSocketOfDoom/Startup.cs
+++ API.fedex/WebSocketOfDoom/WebSocketOfDoom/Startup.cs
@@ -17,47 +17,19 @@ namespace WebSocketOfDoom
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.AddMvc();
-
-            services.AddCors(options => options.AddPolicy("CorsPolicy",
-            builder =>
-            {
-                builder.AllowAnyMethod().AllowAnyHeader()
-                       .WithOrigins("http://localhost:55830")
-                       .AllowCredentials();
-            }));
-
-            services.AddSignalR();
+            services.AddMvc();  
+            services.AddSignalR()
+                .AddAzureSignalR();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-            app.UseCors("CorsPolicy");
-            app.UseSignalR(routes =>
+            app.UseMvc();
+            app.UseFileServer();
+            app.UseAzureSignalR(routes =>
             {
                 routes.MapHub<MapDefinitionHub>("/mapDefinitionHub");
             });
-            app.UseMvc();
         }
     }
 }
