ebed86c1257bf121ac97ad07c1267a3b08b35c11
sam <sagosto@selectintl.com>
2018-05-24 11:27:26 -0400

    SignalR is all up in this

---
+++ API.fedex/WebSocketOfDoom/WebSocketOfDoom/MapDefinitionHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebSocketOfDoom
+{
+    public class MapDefinitionHub : Hub
+    {
+        public async Task SendMap(string user, object map)
+        {
+            await Clients.All.SendAsync("ReceiveMap", user, map);
+        }
+    }
+}
