70805dc9cf5e0b71da84e17a3d368645450248b3
Amanda Zotter <akraemer@selectintl.com>
2018-05-24 16:42:25 -0400

    adding thumbnails to menu

--- Client.fedex/Gridlock/src/app/grid/grid-layout/grid-layout.component.ts
+++ Client.fedex/Gridlock/src/app/grid/grid-layout/grid-layout.component.ts
@@ -42,14 +42,14 @@ export class GridLayoutComponent implements OnInit {
 
 
 
-    this.menuItems.push({label: 'Assign Terrain', icon: 'fa-compass', 
+    this.menuItems.push({label: 'Assign Terrain',
       items: [
-        {label: 'Land 1', styleClass: 'land1', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_01.png'); }},
-        {label: 'Land 2', styleClass: 'land2', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_02.png'); }},
-        {label: 'Land 3', styleClass: 'land3', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_03.png'); }},
-        {label: 'Land 4', styleClass: 'land4', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_04.png'); }},
-        {label: 'Land 5', styleClass: 'land5', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_05.png'); }},
-        {label: 'Land 6', styleClass: 'land6', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_06.png'); }}
+        {label: 'Quicksand', styleClass: 'land1', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_01.png'); }},
+        {label: 'Bluegrass', styleClass: 'land2', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_02.png'); }},
+        {label: 'Ooze', styleClass: 'land3', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_03.png'); }},
+        {label: 'Stone Tile', styleClass: 'land4', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_04.png'); }},
+        {label: 'Lava', styleClass: 'land5', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_05.png'); }},
+        {label: 'Wood Chips', styleClass: 'land6', command: (event) => { this.setTile(this._tileTypes.TERRAIN_TYPE, './assets/land_06.png'); }}
       ]
     });
   }
